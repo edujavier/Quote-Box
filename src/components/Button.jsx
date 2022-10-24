34db1f7bb1c425f485a77a6cb9b5681b8044cf4b
edujavier <delpozoeduardo@hotmail.com>
2022-10-24 13:44:48 -0500

    cambio

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 //import quotes from '../assets/quotes.json'
-const Button = ({changeWord, styleColor}) => {
+const Button = ({changeWord}) => {
   
   
   return (
-    <div>
-      <button className="button" onClick={changeWord} style={{color:styleColor}}><i class="fa-solid fa-chevron-right"></i></button>
+    <div >
+      <button   className="button" onClick={changeWord} ><i class="fa-solid fa-chevron-right"></i></button>
     </div>
   );
 };
--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -3,8 +3,18 @@ import quotes from "../assets/quotes.json"
 import Button from './Button';
 
 
-const QuoteBox = (styleColor) => {
- 
+const QuoteBox = () => {
+  const colors = [
+    '#CB4707',
+    '#B99C3D',
+    '#E7B1AC',
+    '#BE783A',
+    '#6E24F3',
+    '#780DD8'
+  ];
+  
+    const colorIndex = Math.floor(Math.random()*colors.length);
+    document.body.style = `background: ${colors[colorIndex]}`;
   console.log(quotes);
   const randomIndex = Math.floor(Math.random()*quotes.length);
   const[index, setIndex] = useState(randomIndex);
@@ -13,13 +23,13 @@ const QuoteBox = (styleColor) => {
     setIndex(randomIndex2);
   }
   return (
-    <div>
+    <div style={{color: colors[colorIndex]}} >
       <h2 >
       <i class="fa-solid fa-quote-left"></i>
       {quotes[index].quote}</h2>
       <p>{quotes[index].author}</p>
       <div className="button">
-      <Button styleColor={styleColor} changeWord={changeWord}/>
+      <Button changeWord={changeWord} />
       </div>
       
       
